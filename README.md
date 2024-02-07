bc5279295054ca408003f06e856a2e77a4528e53
Eduardo Castro <eduardo.alencar.castro@gmail.com>
2024-02-07 15:44:07 -0300

    Projeto Finalizado

---
+++ README.md
@@ -0,0 +1,6 @@
+# PROJETO DIO - Trilha Java Básico
+
+Projeto de criação de uma simulação de conta corrente básica para treinar conceitos de java.
+
+### Autor
+-Eduardo Castro - www.eduardocastro.site
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int numero;
+        String agencia;
+        String cliente;
+        double saldo;
+        final DecimalFormat df = new DecimalFormat("0.00");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        agencia = scan.nextLine();
+        System.out.println("Por favor, digite o número da sua Conta !");
+        numero = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Por favor, digite o seu nome !");
+        cliente = scan.nextLine();
+        System.out.println("Por favor, digite o valor do seu saldo !");
+        saldo = scan.nextDouble();
+
+    System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +" , conta "+ numero + " e seu saldo " + df.format(saldo) + " já está disponível para saque");
     }
 }
